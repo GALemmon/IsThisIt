7869779c3dab35616b36b5f89eef5d5c59727332
George Lemmon <george.lemmon@gmail.com>
2022-04-21 13:27:09 -0600

    Add welcome page

---
+++ is-this-it/src/Components/Welcome/Welcome.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Welcome.css'
+
+const Welcome = () => {
+
+  return (
+    <div>
+      <header>
+        <h1>TITLE!!!</h1>
+      </header>
+      <section className='welcome-message'>
+        <h2>Hi there!</h2>
+        <p>Heres some stuff about this thing!</p>
+        <Link to="/main">
+          <button className='enter-button'>Click to proceed.</button>
+        </Link>
+      </section>
+    </div>
+  )
+
+}
+
+export default Welcome
