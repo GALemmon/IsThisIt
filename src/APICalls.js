af9b86b864b13fc3811451d20268bb17f63b9e94
George Lemmon <91761276+GALemmon@users.noreply.github.com>
2022-04-21 12:21:34 -0600

    Delete src directory

--- src/APICalls.js
+++
@@ -1,8 +0,0 @@
-import APIKey from "./APIKey"
-
-const fetchNEOs = (startDate, endDate) => {
-  return fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${APIKey}`)
-
-}
-
-export default fetchNEOs
--- src/APIKey.js
+++
@@ -1,3 +0,0 @@
-const APIKey = 'zhGT3RIlCkxud4AQ6QfeUnmGIS1HofyIbRpoXaim'
-
-export default APIKey
