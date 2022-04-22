b3cb3b55b4d1ccae1c6edd8e4bd2d1da7e60c314
George Lemmon <george.lemmon@gmail.com>
2022-04-22 15:14:27 -0600

    Add sort props to main

--- is-this-it/src/Main/Main.js
+++ is-this-it/src/Main/Main.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Main.css'
 import Form from '../Components/Form/Form'
 import NEOsArea from '../Components/NEOsArea/NEOsArea'
 
-const Main = ({ NEOs, errorMessage }) => {
+const Main = ({
+  NEOs,
+  errorMessage,
+  setSortCriteria,
+  sortCriteria
+}) => {
+
   return (
     <>
       <header>
         <h1>TITLE!!!</h1>
-        <Form></Form>
+        <Form setSortCriteria={setSortCriteria} sortCriteria={sortCriteria} />
       </header>
       <main>
-        {NEOs && <NEOsArea NEOs={NEOs} errorMessage={errorMessage} />}
+        {!!NEOs && (
+          <NEOsArea
+            NEOs={NEOs}
+            errorMessage={errorMessage}
+            sortCriteria={sortCriteria}
+          />
+        )}
       </main>
     </>
   )
