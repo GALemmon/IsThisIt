d696f8a4c401ba4b972472309970e798cae22184
George Lemmon <george.lemmon@gmail.com>
2022-04-24 11:50:37 -0600

    Remove broken sort options and console logs

--- is-this-it/src/Components/NEOsArea/NEOsArea.js
+++ is-this-it/src/Components/NEOsArea/NEOsArea.js
@@ -1,13 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './NEOsArea.css'
 import Card from '../Card/Card.js'
-import {
-  sortByDate,
-  sortByDangerous,
-  sortBySizeBiggest,
-  sortBySizeSmallest,
-  sortByClosest
-} from '../../utils'
+import { sortByDate, sortByDangerous, sortByClosest } from '../../utils'
 
 const NEOsArea = ({ NEOs, errorMessage, sortCriteria }) => {
   const [objects, setObjects] = useState(NEOs)
@@ -16,26 +10,15 @@ const NEOsArea = ({ NEOs, errorMessage, sortCriteria }) => {
 
   useEffect(() => {
     sortObjects(objects, sortCriteria)
-    logShit('sort shit:')
   }, [])
 
   useEffect(() => {
     sortObjects(objects, sortCriteria)
-    logShit('re-sort')
   }, [sortCriteria])
 
   useEffect(() => {
     makeCards(sortedObjects)
-    logShit('makin cards')
   }, [sortCriteria, sortedObjects])
-
-  const logShit = (num) => {
-    console.log(`logShit ${num}`)
-    console.log('sortCriteria: ', sortCriteria)
-    console.log('objects: ', objects[0])
-    console.log('sortedObjects: ', sortedObjects[0])
-    console.log('cards: ', cards[0])
-  }
 
   const makeCards = (sortedObjects) => {
     let newCards = sortedObjects.map((object) => {
@@ -50,12 +33,6 @@ const NEOsArea = ({ NEOs, errorMessage, sortCriteria }) => {
       setSortedObjects(newSort)
     } else if (sortCriteria === 'dangerous') {
       let newSort = sortByDangerous(objects)
-      setSortedObjects(newSort)
-    } else if (sortCriteria === 'size-b-to-l') {
-      let newSort = sortBySizeBiggest(objects)
-      setSortedObjects(newSort)
-    } else if (sortCriteria === 'size-l-to-b') {
-      let newSort = sortBySizeSmallest(objects)
       setSortedObjects(newSort)
     } else if (sortCriteria === 'closest') {
       let newSort = sortByClosest(objects)
