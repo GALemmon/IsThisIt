03214ff12aa136dc9daeb0f04f02198ae4c91385
George Lemmon <george.lemmon@gmail.com>
2022-04-24 12:30:17 -0600

    Add styling and text to welcome

--- is-this-it/src/Components/Welcome/Welcome.js
+++ is-this-it/src/Components/Welcome/Welcome.js
@@ -4,13 +4,22 @@ import './Welcome.css'
 
 const Welcome = () => {
   return (
-    <div>
+    <div className='welcome-page'>
       <header>
-        <h1>TITLE!!!</h1>
+        <h1>Is This It?</h1>
       </header>
       <section className='welcome-message'>
-        <h2>Hi there!</h2>
-        <p>Heres some stuff about this thing!</p>
+        <h2>Welcome to "Is This It"!</h2>
+        <p>
+          This site is a project intended to demonstrate technical compentency.
+          While it does utilize a NASA database to populate a list of Near Earth
+          Objecst (NEOs), there is no attempt at any sort of scientific
+          analysis, rigorous or otherwise. Please do not place any stock in the
+          data presented as anything other than a source of mild amusement.
+          <br />
+          <br />
+          Thank you.
+        </p>
         <Link to='/main'>
           <button className='enter-button'>Click to proceed.</button>
         </Link>
