27b46f52bc4c412cf3b0598519f6e1ae388bf114
George Lemmon <george.lemmon@gmail.com>
2022-04-24 21:07:21 -0600

    Move main to components and fix imports

---
+++ is-this-it/src/Components/Main/Main.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Main.css'
+import Form from '../Form/Form'
+import NEOsArea from '../NEOsArea/NEOsArea'
+
+const Main = ({ NEOs, errorMessage, setSortCriteria, sortCriteria }) => {
+  return (
+    <>
+      <header className='main-header'>
+        <h1>Is This It</h1>
+        <Form setSortCriteria={setSortCriteria} sortCriteria={sortCriteria} />
+      </header>
+      <main>
+        {errorMessage && <div className='error'>{errorMessage}</ div>}
+        {!errorMessage && !NEOs && (
+          <div className='loading-mesage'>
+            <h1>Loading! Please stand by!</h1>
+          </div>
+        )}
+        {!!NEOs && (
+          <NEOsArea
+            NEOs={NEOs}
+            errorMessage={errorMessage}
+            sortCriteria={sortCriteria}
+          />
+        )}
+      </main>
+    </>
+  )
+}
+
+export default Main
