cccdfb5d559ddb281d89151c926453916118b6b6
George Lemmon <george.lemmon@gmail.com>
2022-04-24 20:38:20 -0600

    Add cypress stuff and minor edit

--- is-this-it/src/Main/Main.js
+++ is-this-it/src/Main/Main.js
@@ -7,10 +7,11 @@ const Main = ({ NEOs, errorMessage, setSortCriteria, sortCriteria }) => {
   return (
     <>
       <header className='main-header'>
-        <h1>Is This It?</h1>
+        <h1>Is This It</h1>
         <Form setSortCriteria={setSortCriteria} sortCriteria={sortCriteria} />
       </header>
       <main>
+        {errorMessage && <div className='error'>{errorMessage}</ div>}
         {!errorMessage && !NEOs && (
           <div className='loading-mesage'>
             <h1>Loading! Please stand by!</h1>
